f47a83ff248a7053ab1e904cbce957764cffadd6
Mahm0udbakir <mahmoud.m.bakir@gmail.com>
2023-10-10 19:58:26 -0700

    “a”

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * main - check the code
  * Return: Always 0.
  */
 int main(void)
@@ -10,3 +9,4 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
+
